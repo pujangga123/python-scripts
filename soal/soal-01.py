925420905ff795aac8924d23826f03409a98e1f5
En Tay <rush_x@yahoo.com>
2024-11-29 22:49:29 +0700

    a

---
+++ soal/soal-01.py
@@ -0,0 +1,18 @@
+nilai = int(input("Nilai?"))
+
+if nilai >= 6:
+    print("Besar")
+else:
+    print("Kecil")
+
+# Contoh 1:
+# Nilai? 3
+# Kecil
+
+# Contoh 2:
+# Nilai? 9
+# Besar
+
+# Contoh 3:
+# Nilai? 6
+# Kecil
